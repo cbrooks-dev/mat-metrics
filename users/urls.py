ef8c5cc4df2a48643988b9b44e26f68c5847cdb7
cbrooks-dev <connorbrooks04@yahoo.com>
2025-11-24 19:56:50 -0500

    Start adding auth

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #  TODO: add paths like -> path("", views.index, name="index"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
 ]
